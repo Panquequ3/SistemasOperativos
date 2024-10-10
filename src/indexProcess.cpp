c435dd115457fb1e30e9c81ce9132cd8ef58d6b6
Panqueque <108034714+Panquequ3@users.noreply.github.com>
2024-10-09 23:15:45 -0300

    Comentado funcion que abre y lee archivos

--- src/indexProcess.cpp
+++ src/indexProcess.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <map>
+// #include <sstream> // Para istringstream
+// #include <filesystem> //para explorar archivos en una carpeta
 
 using namespace std;
 
@@ -20,3 +22,33 @@ map<string, string>indexCreator(){
     pepe["pablo"] = "peine";
     return pepe;
 }
+
+// void procesa(string carpetaP, string carpetaR, string extension) {
+//     extension = "." + extension;
+//     // Itera a través de todos los archivos con en la carpeta indicada a procesar
+//     //obs: "auto" te permite declarar variables sin especificar explícitamente su tipo
+//     for (const auto& entrada : filesystem::directory_iterator(carpetaP)) {
+//         if (entrada.path().extension() == extension) {
+//             //cout << "Nombre del archivo: " << entrada.path().filename() << endl;
+
+//             ifstream archivoEntrada(entrada.path());  // Abre el archivo para procesarlo (lo abre modo lectura)
+
+//             if (!archivoEntrada) { // Verifica si el archivo se abrió correctamente
+//                 cerr << "No se pudo abrir el archivo para lectura." << endl;
+//                 continue; //instrucción de control de flujo en los bucles que sirve para saltarse el resto del código dentro del 
+//                         //bucle para la iteración actual y pasar inmediatamente a la siguiente iteración.
+//             }
+//             string linea;
+//             while (getline(archivoEntrada, linea)) { // Lee el archivo línea por línea
+//                 // Aqui puedes hacer lo que sea con la linea
+//                 istringstream stream(linea); // Convierte la línea en un stream para procesar palabra por palabra
+//                 string palabra;
+//                 while (stream >> palabra) { // Extrae cada palabra de la línea
+//                     //aqui puedes hacer algo con la palabra donde estas posicionado
+//                 }
+//             }
+//             archivoEntrada.close();
+
+//         }
+//     } 
+// }
