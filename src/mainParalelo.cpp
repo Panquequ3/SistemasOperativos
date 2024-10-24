5ca043714ca6b6088a79c6b441afacf8a6f6fc6d
Panqueque <108034714+Panquequ3@users.noreply.github.com>
2024-10-24 20:04:34 -0300

    se comenzó a crear el  ejecutador

--- src/mainParalelo.cpp
+++ src/mainParalelo.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "conteoParalelo.h"
+#include <chrono> //para la medición del tiempo
 using namespace std;
 
 int main(int argc, char* argv[]){
+    // inicio de la medición
+    auto start = chrono::high_resolution_clock::now();
+
     //En caso de reutilizarlo en otra parte
 	if(argc != 8){
 		cout << "Error. Debe ejecutarse como ./paralelo nHilos pathP pathR extension pathStopWord pathTemp mapPath" << endl;
@@ -18,5 +22,15 @@ int main(int argc, char* argv[]){
 
     //ej : assignThreads(4, "./data/procesar", "./data/resultados","txt", "./data/stop_word.txt", "./data/temporal", "./data/map.txt");
     assignThreads(numTh, pathP, pathR, ext, stopWord, temp, map);
+    // para finalizar el tiempo de medición
+    auto end = chrono::high_resolution_clock::now();
+
+    // calculamos la duración
+    // chrono::duration -> representa un intervalo de tiempo , definte <tipoDato, unidadTiempo>
+    chrono::duration<double, milli> duration = end - start;
+    cout << "La duración fue " << duration.count() << " ms " <<  endl;
+    // Duración en segundos
+    chrono::duration<double> duration_s = end - start;
+    cout << "Duración en segundos: " << duration_s.count() << " s" << endl;
     return 0;
 }
