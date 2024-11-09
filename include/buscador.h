43351084ca918c522d27f26c9856950ce204701e
Panqueque <108034714+Panquequ3@users.noreply.github.com>
2024-11-09 00:49:51 -0300

    modificado el buscador con socket

--- include/buscador.h
+++ include/buscador.h
@@ -4,7 +4,9 @@
 #include "motorBusqueda.h"
 using namespace std;
 
+void startClient(string map_path,string outputWord);
 void searcher(string outputWord);
+void printResults(vector<string> vec);
 vector<string> split(string str);
 string searchOnMap(string mapPath, string id);
 string translate(string answer, string mapPath);
--- src/buscador.cpp
+++ src/buscador.cpp
@@ -1,34 +1,77 @@
 #include <iostream> 
 #include <string> 
 #include <vector>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <cstring>
 #include "motorBusqueda.h"
 #include <laserpants/dotenv/dotenv.h>
 
 using namespace std;
 
-const string map_path = "MAP_PATH";
+const string INTERMEDIARY_SOCKET_PATH = "./data/socket/socket_11";
 
+void startClient(string map_path, string outputWord = "SALIR AHORA") {
+    int client_fd;
+    sockaddr_un address;
+    char buffer[1048] = {0};
 
-// imprime el menu y espera la frase
-void searcher(string outputWord = "SALIR AHORA"){
-    string inputPhrase;
+    client_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (client_fd < 0) {
+        cerr << "Error al crear el socket del cliente" << endl;
+        return;
+    }
+
+    address.sun_family = AF_UNIX;
+    strncpy(address.sun_path, INTERMEDIARY_SOCKET_PATH.c_str(), sizeof(address.sun_path) - 1);
+
+    if (connect(client_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        cerr << "Error al conectar con el intermediario" << endl;
+        close(client_fd);
+        return;
+    }
+    
+    //aqui va la definicion de cosas dentro de la interfaz si llegase a ser necesario
+
+    string message;
+
+    //aqui se imprime el mensaje de la interfaz (parte visual)
     cout << "+---------------------------+ " << endl;
     cout << "| ¡Bienvenido al buscador!  |" << endl;
     cout << "+---------------------------+ " << endl;
     cout << "(Si desea salir, escriba: '" << outputWord << "')" << endl;
 
-    while(inputPhrase != outputWord){
+    while(true){
         cout <<  "\ningrese la frase a buscar: ";
-        cin >> inputPhrase;
-        //llamamos al cache y al motor de busqueda
-        string answer = ""; // respuesta que recibe del cache
-        vector<string> = translate(answer, map_path);
+        cin >> message;
+        
+        if(message == outputWord){
+            cout << "¡¡Hasta pronto!!" << endl;
+            send(client_fd, outputWord.c_str(), message.size(), 0);
+            break;
+        }
+
+        // Enviar mensaje al intermediario (AKA cache)
+        send(client_fd, message.c_str(), message.size(), 0);
+
+        // Leer la respuesta del intermediario
+        memset(buffer, 0, sizeof(buffer));
+        int bytesRead = read(client_fd, buffer, sizeof(buffer) - 1);
+        if (bytesRead > 0) {
+            cout << "Cliente recibió del Intermediario: " << buffer << endl;
+            string answer;
+            answer.asing(buffer); // respuesta que recibe del cache
+            vector<string> results = translate(answer, map_path);
+            printResults(results);
+        }
+
     }
-    if(inputPhrase == outputWord)
-        cout << "¡¡Hasta pronto!!" << endl;
 
-
+    close(client_fd);
 }
+
+
 // imprime los resultados del vector
 void printResults(vector<string> vec) {
     for (string str : vec) {
@@ -96,19 +139,19 @@ vector<string> translate(string answer, string mapPath){
         id = extractId(elem).substr(2);
         score = extractCant(elem);
         book = searchOnMap(mapPath, id);
-        results.push_back(to_string(position) + ") " + "("+ score + ") " + book );
+        results.push_back(to_string(position) + ") " + "("+ to_string(score) + ") " + book );
         position++;
     }
     return results;
 }
 
 int main(int argc, char* argv[]){
-	// if(argc != n){
-	// 	cout << "Error. Debe ejecutarse como ./buscador " << endl;
-	// 	exit(EXIT_FAILURE);
-	// }
-
-    // buscador probablemente deba ser un cliente.. o utilizar colas de archivos
+	if(argc != 2){
+		cout << "Error. Debe ejecutarse como ./busc mapPath" << endl;
+		exit(EXIT_FAILURE);
+	}
+    string map_path = argv[1];
+    startClient(map_path);
     return 0;
 }
     // Deberiamos asegurar que se ejecuto la opcion que crea el mapa_Archivos??
